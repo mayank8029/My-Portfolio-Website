ffd07cf1b60111d9712c0eba5372e81197155778
Mayank <mayankjhanwar29@gmail.com>
2024-01-23 21:44:10 +0530

    add navabar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import {Routes , Route} from "react-router-dom"
+
+import {About , Contact , Experience , Feedbacks , Hero , Navbar , Tech , Works , StarsCanvas} from './components'
+
 
 const App = () => {
+  console.log(StarsCanvas)
   return (
-    <div>App</div>
+    <div className="relative z-0 bg-primary">
+    <div className='bg-hero-pattern bg-cover , bg-no-repeat bg-center'>
+    <Navbar/>
+    <Hero/>
+    </div>
+    <About/>
+    <Experience/>
+    <Tech/>
+    <Works/>
+    <Feedbacks/>
+
+    <div className='relative z-0'>
+    <Contact/>
+    <StarsCanvas/>
+    </div>
+
+    </div>
   )
 }
 
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React , {Suspense , useEffect , useState} from 'react'
+
+
 
 const Computers = () => {
   return (
